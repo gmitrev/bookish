6e5b7ba8fb8aa167ae7f370e9a4fb372bb0a0cb3
georgi <gvmitrev@gmail.com>
2013-05-26 15:15:28 +0300

    Interface improvements + turbolinks

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,5 @@ class Author < ActiveRecord::Base
   attr_accessible :born, :died, :name, :nationality
 
   has_many :authorships
-  has_many :books, through: :authorships
+  has_many :books, through: :authorships, uniq: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,16 @@
 class Book < ActiveRecord::Base
-  attr_accessible :description, :isbn, :lang, :page_count, :published_by, :published_in, :title
+  attr_accessible :description, :isbn, :lang, :page_count, :published_by, :published_in, :title, :author
 
-  has_many :authorships
-  has_many :authors, through: :authorships
+  attr_accessor :author
+
+  has_many :authorships, dependent: :destroy
+  has_many :authors, through: :authorships, uniq: true
+
+  before_save do
+    old_author = self.authors.first
+    new_author = Author.find(author)
+    if old_author != new_author
+      self.authors = [new_author]
+    end
+  end
 end
